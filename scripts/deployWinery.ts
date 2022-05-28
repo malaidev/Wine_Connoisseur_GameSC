73f9f3b66787150effd2d256a10f33ec1b999d08
bitmaster001 <superora0002@gmail.com>
2022-05-28 10:05:07 -0400

    winery contract - upgradeable

--- scripts/deployWinery.ts
+++ scripts/deployWinery.ts
@@ -1,23 +1,46 @@
-import {ethers} from 'hardhat'
+import { ethers, upgrades } from "hardhat";
 
 async function main(): Promise<string> {
-    const [deployer] = await ethers.getSigners()
-    if (deployer === undefined) throw new Error('Deployer is undefined.')
-    
-    console.log('Account balance:', (await deployer.getBalance()).toString())
+  const [deployer] = await ethers.getSigners();
+  if (deployer === undefined) throw new Error("Deployer is undefined.");
 
-    const Winery = await ethers.getContractFactory('Winery')
-    const Winery_Deployed = await Winery.deploy()
+  console.log("Account balance:", (await deployer.getBalance()).toString());
 
-    return Winery_Deployed.address;
+  const Winery = await ethers.getContractFactory("Winery");
+  //   const WineryDeployed = await Winery.deploy();
+  const WineryDeployed = await upgrades.deployProxy(Winery, {
+    initializer: "initialize",
+  });
+  await WineryDeployed.deployed();
+
+  return WineryDeployed.address;
 }
 
 main()
-    .then((r: string) => {
-        console.log("deployed address:", r);
-        return r;
-    })
-    .catch(error => {
-        console.error(error)
-        process.exit(1)
-    })
+  .then((r: string) => {
+    console.log("deployed address:", r);
+    return r;
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+// //available functions
+// async function main() {
+//   const [deployer] = await ethers.getSigners()
+//   if (deployer === undefined) throw new Error('Deployer is undefined.')
+//   console.log('Deploying contracts with the account:', deployer.address)
+
+//   console.log('Account balance:', (await deployer.getBalance()).toString())
+//  const Token = await ethers.getContractFactory("LIFEGAMES");
+
+//  console.log("Deploying upgradeable contract of Token...");
+
+//  const TokenDeployed = await upgrades.deployProxy(Token, {
+//   initializer: "initialize",
+//  });
+//  await TokenDeployed.deployed();
+
+//  console.log("Contract deployed to:", TokenDeployed.address);
+// }
